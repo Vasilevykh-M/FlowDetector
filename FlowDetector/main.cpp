dec9588042c1ef20db9181d00290f4cc69b2046e
Vasilevykh-M <vasilevyh00@gmail.com>
2025-08-10 17:07:08 +0500

    fix args main and update README.md

--- FlowDetector/main.cpp
+++ FlowDetector/main.cpp
@@ -4,15 +4,15 @@
 
 int main(int argc, char* argv[]) 
 {
-    if(argc != 3)
+    if(argc != 4)
         return -1;
 
-    std::string input_dir = argv[0];
-    std::string output_dir = argv[1];
+    std::string input_dir = argv[1];
+    std::string output_dir = argv[2];
 
     std::vector <cv::String > filenames = get_files(input_dir);
 
-    FlowDetector flow_detector = FlowDetector(argv[2], cv::Size(80, 80), 25);
+    FlowDetector flow_detector = FlowDetector(argv[3], cv::Size(80, 80), 25);
 
     if (filenames.empty()) {
         std::cerr << "No image files found in " << input_dir << std::endl;
